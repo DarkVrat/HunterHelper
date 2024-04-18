d8a345db496729663654e147e490127453fab908
DarkVrat <118006878+DarkVrat@users.noreply.github.com>
2024-04-18 15:45:18 +0500

    add first lua script

--- HunterHelper/HunterHelper.lua
+++ HunterHelper/HunterHelper.lua
@@ -1,1 +1,9 @@
-print("HunterHelper load")
+local frame = CreateFrame("FRAME");
+
+function frame:OnEvent(event, ...)
+        CastSpellByID(19506);
+        CastSpellByID(14322);
+end
+
+frame:RegisterEvent("PLAYER_REGEN_DISABLED");
+frame:SetScript("OnEvent", frame.OnEvent);
